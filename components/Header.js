11cbd8073a51ecccf23c1d84c702a2559e2e0ab4
aditya19chavan <155563749+aditya19chavan@users.noreply.github.com>
2024-11-29 01:05:13 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -34,37 +34,64 @@ const Header = () => {
                 About
               </a>
             </li>
+
+                  // start
             <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle text-light"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Services/Products
-              </a>
-              <ul class="dropdown-menu">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Category 1
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Category 2
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu dropdown-menu-dark p-4" style="width: 1000px;">
+                    <div class="row">
+                      <!-- Column 1 -->
+                      <div class="col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a class="dropdown-item" href="#">About HPE</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Learn about HPE</a></li>
+                          <li><a class="dropdown-item" href="#">Corporate social responsibility</a></li>
+                          <li><a class="dropdown-item" href="#">Careers</a></li>
+                          <li><a class="dropdown-item" href="#">Events</a></li>
+                        </ul>
+                      </div>
+                      <!-- Column 2 -->
+                      <div class="col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a class="dropdown-item" href="#">Education and training</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">HPE education services</a></li>
+                          <li><a class="dropdown-item" href="#">Certification and learning</a></li>
+                          <li><a class="dropdown-item" href="#">developer workshops</a></li>
+                          <li><a class="dropdown-item" href="#"> technical courses</a></li>
+                        </ul>
+                      </div>
+                      <!-- Column 3 -->
+                      <div class="col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a class="dropdown-item" href="#">Customer success stories</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Reference architectures</a></li>
+                          <li><a class="dropdown-item" href="#">Quick specs</a></li>
+                          <li><a class="dropdown-item" href="#">Webinars</a></li>
+                          <li><a class="dropdown-item" href="#">See all</a></li>
+                        </ul>
+                      </div>
+                      <!-- Column 4 -->
+                      <div class="col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a class="dropdown-item" href="#">Getting started with HPE</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Get to Know HPE GreenLake</a></li>
+                          <li><a class="dropdown-item" href="#">HPE GreenLake FAQ</a></li>
+                          <li><a class="dropdown-item" href="#">HPE GreenLake user guide</a></li>
+                          <li><a class="dropdown-item" href="#">Sign up for free</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  
+              </li>
+
+                  //end
             <li class="nav-item">
               <a class="nav-link text-light" href="#">
                 Connect
